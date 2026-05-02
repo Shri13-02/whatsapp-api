195b8169683222647be209ec8fa0f2b4bc8b9feb
Shri13-02 <166051818+Shri13-02@users.noreply.github.com>
2026-05-02 07:28:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.post("/send", async (req, res) => {
         to: TO,
         type: "template",
         template: {
-          name: "hello world",
+          name: "hello_world",
           language: {
             code: "en_US"
           }
